6ee3a252344ecfbe6e298b4ff524ca811af6f64b
Sam Maloney <samaloney@live.ca>
2019-11-04 18:52:56 +0000

    initial (incorrect) attempt at solving for FEM solution; also added some plotting

--- FEM.py
+++ FEM.py
@@ -2,12 +2,12 @@
 
 import numpy as np
 from numpy.linalg import inv, det
-#import matplotlib as mpl
-#import matplotlib.pyplot as plt
-#from scipy.sparse import csr_matrix
-#from scipy.sparse.linalg import cg
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import cg
 
-from math import sqrt
+from math import sqrt, sin, sinh, pi
 
 class Mesh(object):
     """ Simple Mesh class for a regular triangular grid on a unit square
@@ -17,6 +17,8 @@ class Mesh(object):
             for i in range(self.N + 1):
                 self.nodes[j*(self.N+1) + i, 0] = i/self.N
                 self.nodes[j*(self.N+1) + i, 1] = j/self.N
+                if (i == 0) or (j == 0) or (i == self.N) or (j == self.N):
+                    self.is_boundary_node[j*(self.N+1) + i] = True
         
         for j in range(self.N):
             for i in range(self.N):
@@ -32,6 +34,7 @@ class Mesh(object):
         self.N = N
         self.elems = np.empty((2*N*N      , 3), dtype='int32')
         self.nodes = np.empty(((N+1)*(N+1), 2), dtype='float64')
+        self.is_boundary_node = np.full((N+1)*(N+1), False)
         self.generate_mesh()
     
     def __repr__(self):
@@ -72,24 +75,40 @@ class Mesh(object):
         
 
 ##### Start of main program #####
+        
+# wavenumber for boundary function u(x,1) = sinh(k*pi) = g(x,y)
+k = 1
 
 # Numer of grid divisions along one dimension
 # Number of triangular elements equals 2*N*N
-N = 1
+N = 2
 mesh = Mesh(N)
 
 # pre-allocate arrays for stiffness matrix triplets
-data = np.empty(9*2*N*N)
-row_ind = np.empty(9*2*N*N)
-col_ind = np.empty(9*2*N*N)
+data = np.empty(9*2*N*N, dtype='float64')
+row_ind = np.empty(9*2*N*N, dtype='int32')
+col_ind = np.empty(9*2*N*N, dtype='int32')
 
-# pre-allocate array for RHS forcing vector
-b = np.empty((N+1)*(N+1))
+# pre-allocate array for RHS vector and Dirichlet boundary function
+n_nodes = (N+1)*(N+1)
+F = np.zeros(n_nodes, dtype='float64')
+g = np.zeros(n_nodes, dtype='float64')
+
+# compute boundary function values at boundary nodes
+#is_top_boundary = ( (mesh.nodes[:,1] == 1.0)
+#                * (mesh.nodes[:,0] != 0.0)
+#                * (mesh.nodes[:,0] != 1.0) )
+#g[is_top_boundary] = sinh(k*pi)
+
+for i_node, node in enumerate(mesh.nodes):
+    if ( node[1] == 1.0 ):
+        g[i_node] = sin(k*pi*node[0]) * sinh(k*pi)
 
 # define shape-function gradients on reference triangle
-grad_a = np.array([[-1/sqrt(2)], [-1/sqrt(2)]])
-grad_b = np.array([[1], [0]])
-grad_c = np.array([[0], [1]])
+gradients = np.array([[-sqrt(2), -1, 0], [-sqrt(2), 0, -1]])
+#grad_a = np.array([[-sqrt(2)], [-sqrt(2)]])
+#grad_b = np.array([[-1], [0]])
+#grad_c = np.array([[0], [-1]])
 
 # triplet index for assembly
 index = 0
@@ -102,4 +121,79 @@ for elem in mesh:
     J = np.array([N_b - N_a, N_c - N_a]).transpose()
     det_J = det(J)
     J_inv_T = inv(J).transpose()
-#    data[index] = 
+    
+    for i_alpha, alpha in enumerate(elem):
+        if mesh.is_boundary_node[alpha]:
+            # if alpha is on the top boundary, where y = 1
+            if ( (mesh.nodes[alpha,1] == 1.0)
+                    and not (mesh.nodes[alpha,0] == 0.0)
+                    and not (mesh.nodes[alpha,0] == 1.0) ):
+                F[alpha] += g[alpha]
+                # for homogenous case all interior values of F are zero
+                # so this is the only place we add to F
+            for beta in elem:
+                if (beta == alpha):
+                    data[index] = 1
+                    row_ind[index] = alpha
+                    col_ind[index] = beta
+                    index += 1;
+                else:
+                    data[index] = 0
+                    row_ind[index] = alpha
+                    col_ind[index] = beta
+                    index += 1;
+                    
+        # if alpha is not a boundary node
+        else:
+            for i_beta, beta in enumerate(elem):
+                if mesh.is_boundary_node[beta]:
+                    F[alpha] -= ( g[beta] * det_J
+                                * np.dot(J_inv_T@gradients[:,i_alpha],
+                                         J_inv_T@gradients[:,i_beta]) )
+                    data[index] = 0
+                    row_ind[index] = alpha
+                    col_ind[index] = beta
+                    index += 1;
+                else:
+                    data[index] = det_J*np.dot(J_inv_T@gradients[:,i_alpha],
+                                               J_inv_T@gradients[:,i_beta])
+                    row_ind[index] = alpha
+                    col_ind[index] = beta
+                    index += 1;
+
+# assemble the triplets into the sparse stiffness matrix
+A = csr_matrix((data, (row_ind, col_ind)), shape=(n_nodes, n_nodes))
+
+# solve for the approximate solution
+u, info = cg(A, F)
+if (info == 0):
+    print('Solution successful')
+else:
+    print('solution failed with error code:', info)
+
+# compute the analytic solution
+u_exact = np.empty(n_nodes, dtype='float64')
+for i_node, node in enumerate(mesh.nodes):
+    u_exact[i_node] = sin(k*pi*node[0])*sinh(k*pi*node[1])
+
+E_inf = abs(u - u_exact).max()
+print('max error = ', E_inf)
+
+triangulation = mpl.tri.Triangulation(mesh.nodes[:,0],
+                                      mesh.nodes[:,1],
+                                      mesh.elems)
+
+# plot the mesh
+#plt.triplot(triangulation)
+
+# plot the result
+plt.subplot(211)
+plt.tripcolor(triangulation, u, shading='gouraud')
+plt.colorbar()
+
+# plot analytic solution
+plt.subplot(212)
+plt.tripcolor(triangulation, u_exact, shading='gouraud')
+plt.colorbar()
+
+B = A.toarray()
