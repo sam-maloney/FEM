dd2bcc5e23bf58895e70076861dcdee1b567a7ce
Sam Maloney <sam.maloney@warwick.ac.uk>
2019-12-08 20:41:44 +0000

    refactored FEM.py with new class FemSim and made Mesh class more pythonic

--- FEM.py
+++ FEM.py
@@ -8,59 +8,33 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import cg
 
 class Mesh(object):
-    """ Simple Mesh class for a regular triangular grid on a unit square."""
-    
-    def generate_mesh(self):
-        for j in range(self.N + 1):
-            for i in range(self.N + 1):
-                self.nodes[j*(self.N+1) + i, 0] = i/self.N
-                self.nodes[j*(self.N+1) + i, 1] = j/self.N
-                if (i == 0) or (j == 0) or (i == self.N) or (j == self.N):
-                    self.is_boundary_node[j*(self.N+1) + i] = True
-        
-        for j in range(self.N):
-            for i in range(self.N):
-                self.elems[2*j*self.N + 2*i, 0] = (j  )*(self.N+1) + i
-                self.elems[2*j*self.N + 2*i, 1] = (j  )*(self.N+1) + i+1
-                self.elems[2*j*self.N + 2*i, 2] = (j+1)*(self.N+1) + i+1
-                
-                self.elems[2*j*self.N + 2*i+1, 0] = (j  )*(self.N+1) + i
-                self.elems[2*j*self.N + 2*i+1, 1] = (j+1)*(self.N+1) + i+1
-                self.elems[2*j*self.N + 2*i+1, 2] = (j+1)*(self.N+1) + i
+    """ Simple Mesh class for a regular triangular grid on a unit square."""        
     
     def __init__(self, N):
         self.N = N
-        self.elems = np.empty((2*N*N      , 3), dtype='int32')
-        self.nodes = np.empty(((N+1)*(N+1), 2), dtype='float64')
-        self.is_boundary_node = np.full((N+1)*(N+1), False)
-        self.generate_mesh()
+        self.nodes = np.indices((N+1, N+1)).T.reshape(-1,2) / N
+        self.is_boundary_node = np.any(np.mod(self.nodes, 1) == 0, axis=1)
+        self.elems = np.empty((2*N*N, 3), dtype='int32')
+        for j in range(N):
+            for i in range(N):
+                self.elems[2*j*N + 2*i, 0] = (j  )*(N+1) + i
+                self.elems[2*j*N + 2*i, 1] = (j  )*(N+1) + i+1
+                self.elems[2*j*N + 2*i, 2] = (j+1)*(N+1) + i+1
+                
+                self.elems[2*j*N + 2*i+1, 0] = (j  )*(N+1) + i
+                self.elems[2*j*N + 2*i+1, 1] = (j+1)*(N+1) + i+1
+                self.elems[2*j*N + 2*i+1, 2] = (j+1)*(N+1) + i
     
     def __repr__(self):
-        return self.__class__.__name__ + '({!r})'.format(self.N)
+        return '{}({!r})'.format(self.__class__.__name__, self.N)
 
     def __str__(self):
         string = repr(self) + '\n\nNodes:\n#\tx\ty\n'
-        for j in range(self.N + 1):
-            for i in range(self.N + 1):
-                n = j*(self.N+1) + i
-                string += (str(n) + '\t'
-                           + "{:.5f}".format(self.nodes[n, 0]) + '\t'
-                           + "{:.5f}".format(self.nodes[n, 1]) + '\n')
-        
+        for n, (x, y) in enumerate(self.nodes):
+            string += "{:d}\t{:.5f}\t{:.5f}\n".format(n, x, y)
         string += '\nElements:\n#\tn1\tn2\tn3\n'
-        for j in range(self.N):
-            for i in range(self.N):
-                n = 2*j*self.N + 2*i
-                string += (str(n) + '\t'
-                           + str(self.elems[n, 0]) + '\t'
-                           + str(self.elems[n, 1]) + '\t'
-                           + str(self.elems[n, 2]) + '\n')
-                
-                n = 2*j*self.N + 2*i+1
-                string += (str(n) + '\t'
-                           + str(self.elems[n, 0]) + '\t'
-                           + str(self.elems[n, 1]) + '\t'
-                           + str(self.elems[n, 2]) + '\n')
+        for e, (a, b, c) in enumerate(self.elems):
+            string += "{:d}\t{:d}\t{:d}\t{:d}\n".format(e, a, b, c)
         return string
     
     def __len__(self):
@@ -71,54 +45,88 @@ class Mesh(object):
     
 ##### End of Mesh class definition #####
 
-
-##### Begin helper functions #####
-
-def FEM_initialize(N):
-    # this is just a helper function, so we want everything global
-    global mesh, data, row_ind, col_ind, n_nodes, F, g
+class FemSim(object):
+    """Linear FEM simulation class."""
     
-    # generate the mesh
-    mesh = Mesh(N)
-    
-    # pre-allocate arrays for stiffness matrix triplets
-    # these are the maximum possibly required sizes; not all will be used
-    data = np.zeros(9*2*N*N, dtype='float64')
-    row_ind = np.zeros(9*2*N*N, dtype='int32')
-    col_ind = np.zeros(9*2*N*N, dtype='int32')
-    
-    # pre-allocate array for RHS vector and Dirichlet boundary function
-    n_nodes = (N+1)*(N+1)
-    F = np.zeros(n_nodes, dtype='float64')
-    g = np.zeros(n_nodes, dtype='float64')
-    
-    # compute boundary function values at boundary nodes
-    g[mesh.is_boundary_node] = \
-        ( np.sin(k*np.pi*mesh.nodes[mesh.is_boundary_node,0])
-        * np.sinh(k*np.pi*mesh.nodes[mesh.is_boundary_node,1]) )
-
-
-def compute_error(u, iN):
-    global mesh, E_inf, E_2, k
-    
-    u_exact = np.sin(k*np.pi*mesh.nodes[:,0])*np.sinh(k*np.pi*mesh.nodes[:,1])
-    E_inf[iN] = np.linalg.norm(u - u_exact, np.inf)
-    E_2[iN] = np.linalg.norm(u - u_exact)/N
-    print('max error =', E_inf[iN])
-    print('L2 error  =', E_2[iN])
-    
-    return u_exact
+    def __init__(self, N, k=1):
+        self.N = N
+        self.k = k
+        self.mesh = Mesh(N)
         
-##### End helper functions #####
+        # pre-allocate arrays for stiffness matrix triplets
+        # these are the maximum possibly required sizes; not all will be used
+        self.data = np.zeros(9*2*N*N, dtype='float64')
+        self.row_ind = np.zeros(9*2*N*N, dtype='int32')
+        self.col_ind = np.zeros(9*2*N*N, dtype='int32')
+        
+        # pre-allocate array for RHS vector and Dirichlet boundary function
+        self.n_nodes = (N+1)*(N+1)
+        self.F = np.zeros(self.n_nodes, dtype='float64')
+        self.g = np.zeros(self.n_nodes, dtype='float64')
+        
+        # compute boundary function values at boundary nodes
+        self.g[self.mesh.is_boundary_node] = (
+              np.sin(k*np.pi*self.mesh.nodes[self.mesh.is_boundary_node,0])
+            * np.sinh(k*np.pi*self.mesh.nodes[self.mesh.is_boundary_node,1]) )
+    
+    def assembleStiffnessMatrix(self):
+        # define shape-function gradients on reference triangle
+        gradients = np.array([[-1, 1, 0], [-1, 0, 1]])
+        
+        # triplet index for assembly
+        index = 0
+        
+        for elem in self.mesh:
+            N_a = self.mesh.nodes[elem[0]]
+            N_b = self.mesh.nodes[elem[1]]
+            N_c = self.mesh.nodes[elem[2]]
+            # define transformation matrix for elem to reference triangle
+            J = np.array([N_b - N_a, N_c - N_a]).transpose()
+            det_J = np.linalg.det(J)
+            J_inv_T = np.linalg.inv(J).transpose()
+            
+            for i_alpha, alpha in enumerate(elem):
+                if self.mesh.is_boundary_node[alpha]:
+                    self.F[alpha] += self.g[alpha]
+                    for beta in elem:
+                        if (beta == alpha):
+                            self.data[index] = 1
+                            self.row_ind[index] = alpha
+                            self.col_ind[index] = beta
+                            index += 1;
+                    continue # to next alpha iteration
+                # if alpha is not a boundary node
+                for i_beta, beta in enumerate(elem):
+                    if self.mesh.is_boundary_node[beta]:
+                        self.F[alpha] -= ( self.g[beta] * det_J
+                                    * np.dot(J_inv_T@gradients[:,i_alpha],
+                                             J_inv_T@gradients[:,i_beta]) )
+                        continue # to next beta iteration
+                    # if beta is not a boundary node
+                    self.data[index] = det_J*np.dot(J_inv_T@gradients[:,i_alpha],
+                                               J_inv_T@gradients[:,i_beta])
+                    self.row_ind[index] = alpha
+                    self.col_ind[index] = beta
+                    index += 1;
+        
+        # assemble the triplets into the sparse stiffness matrix
+        self.A = csr_matrix((self.data, (self.row_ind, self.col_ind)),
+                            shape=(self.n_nodes, self.n_nodes))
+    
+    def solve(self):
+        """Solve for the approximate solution."""
+        self.u, self.info = cg(self.A, self.F)
+        
+    def __repr__(self):
+        return '{}({!r},{!r})'.format(self.__class__.__name__, self.N, self.k)
+
+##### End of FemSim class definition #####
         
 
 ##### Start of main program #####
         
 # wavenumber for boundary function u(x,1) = sinh(k*pi) = g(x,y)
 k = 1
-
-# define shape-function gradients on reference triangle
-gradients = np.array([[-1, 1, 0], [-1, 0, 1]])
 
 # allocate arrays for convergence testing
 start = 1
@@ -137,56 +145,24 @@ for iN, N in enumerate(N_array):
     print('N =', N)
     
     # allocate arrays and compute boundary values
-    FEM_initialize(N)
+    femSim = FemSim(N, k)
     
-    # triplet index for assembly
-    index = 0
+    # Assemble the stiffness matrix and solve for the approximate solution
+    femSim.assembleStiffnessMatrix()
+    femSim.solve();
     
-    for elem in mesh:
-        N_a = mesh.nodes[elem[0]]
-        N_b = mesh.nodes[elem[1]]
-        N_c = mesh.nodes[elem[2]]
-        # define transformation matrix for elem to reference triangle
-        J = np.array([N_b - N_a, N_c - N_a]).transpose()
-        det_J = np.linalg.det(J)
-        J_inv_T = np.linalg.inv(J).transpose()
-        
-        for i_alpha, alpha in enumerate(elem):
-            if mesh.is_boundary_node[alpha]:
-                F[alpha] += g[alpha]
-                for beta in elem:
-                    if (beta == alpha):
-                        data[index] = 1
-                        row_ind[index] = alpha
-                        col_ind[index] = beta
-                        index += 1;
-                        
-            # if alpha is not a boundary node
-            else:
-                for i_beta, beta in enumerate(elem):
-                    if mesh.is_boundary_node[beta]:
-                        F[alpha] -= ( g[beta] * det_J
-                                    * np.dot(J_inv_T@gradients[:,i_alpha],
-                                             J_inv_T@gradients[:,i_beta]) )
-                    else:
-                        data[index] = det_J*np.dot(J_inv_T@gradients[:,i_alpha],
-                                                   J_inv_T@gradients[:,i_beta])
-                        row_ind[index] = alpha
-                        col_ind[index] = beta
-                        index += 1;
-    
-    # assemble the triplets into the sparse stiffness matrix
-    A = csr_matrix((data, (row_ind, col_ind)), shape=(n_nodes, n_nodes))
-    
-    # solve for the approximate solution
-    u, info = cg(A, F)
-#    if (info == 0):
+#    if (femSim.info == 0):
 #        print('Solution successful')
-    if (info != 0):
-        print('solution failed for N = ', N, ' with error code:', info)
+    if (femSim.info != 0):
+        print('solution failed for N = ', N, ' with error code:', femSim.info)
     
     # compute the analytic solution and error norms
-    u_exact = compute_error(u, iN)
+    u_exact = ( np.sin(femSim.k*np.pi*femSim.mesh.nodes[:,0])
+               *np.sinh(femSim.k*np.pi*femSim.mesh.nodes[:,1]) )
+    E_inf[iN] = np.linalg.norm(femSim.u - u_exact, np.inf)
+    E_2[iN] = np.linalg.norm(femSim.u - u_exact)/N
+    print('max error =', E_inf[iN])
+    print('L2 error  =', E_2[iN])
     
 ##### End of loop over N #####
 
@@ -203,13 +179,13 @@ mpl.rc('ytick', labelsize='large')
 plt.subplots_adjust(hspace = 0.3, wspace = 0.25)
 
 # create a matplotlib triangulation object from the mesh
-triangulation = mpl.tri.Triangulation(mesh.nodes[:,0],
-                                      mesh.nodes[:,1],
-                                      mesh.elems)
+triangulation = mpl.tri.Triangulation(femSim.mesh.nodes[:,0],
+                                      femSim.mesh.nodes[:,1],
+                                      femSim.mesh.elems)
 
 # plot the result
 plt.subplot(221)
-plt.tripcolor(triangulation, u, shading='gouraud')
+plt.tripcolor(triangulation, femSim.u, shading='gouraud')
 plt.colorbar()
 plt.xlabel(r'$x$')
 plt.ylabel(r'$y$')
